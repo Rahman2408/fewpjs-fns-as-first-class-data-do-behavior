9604d11e497a4fb527c9a5d72350063b9447788d
Rahman Elsirafy <rahman2408@gmail.com>
2021-08-30 22:00:40 -0400

    commit tree 31c241535741d1cdb1c594245606fe3018676c52

--- index.js
+++ index.js
@@ -10,4 +10,14 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(x) {
+  let h = parseInt(x.split(':')[0]);
+  let m = parseInt(x.split(':')[1]);
+  var msg;
+  if (h < 12 && h > 0){msg = "Good Morning"}
+  if ( h >= 12 && h <17 ){msg =  "Good Afternoon"}
+  if (h >= 17 ){ msg = "Good Evening"}
+  return msg;
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(str) { document.getElementById("greeting").textContent = str;}
